cb5045f6d63dd7c5dfeee837810c3dc1a28c7d03
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-08-25 11:27:19 -0300

    Added handleclick function

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -2,14 +2,22 @@ import React, { useState } from 'react';
 import './Board.css';
 
 function Board() {
+
   const emptyBoard = Array(9).fill('');
   const [board, setBoard] = useState(emptyBoard);
+  const [currentPlayer, setCurrentPlayer] = useState('O')
+
+  function handleBoardClick(index){
+    setBoard(board.map((element, elementIndex) => elementIndex === index ? currentPlayer : element));
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+  }
+
   return (
     <div className="container">
       <div className="game">
         <div className="board-grid">
           {board.map((element, index) => {
-            return <button key={index} className={`btn ${element}`}>{element}</button>
+            return <button className={`btn ${element}`} key={index} onClick={() => handleBoardClick(index)}>{element}</button>
           })}
           
         </div>
